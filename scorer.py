e99f2d354175d81c16ddd5d97b5ee25d4a9918d0
VAYakushev <yakush_95@mail.ru>
2026-05-14 01:01:03 +0500

    Update scorer.py

--- scorer.py
+++ scorer.py
@@ -1,4 +1,17 @@
-from config import HIGH_PRIORITY_KEYWORDS, MEDIUM_PRIORITY_KEYWORDS
+from config import (
+    HIGH_PRIORITY_KEYWORDS,
+    MEDIUM_PRIORITY_KEYWORDS,
+    BLOCKED_KEYWORDS,
+)
+
+
+def is_article_blocked(article):
+    text = (article["title"] + " " + article["description"]).lower()
+    for kw in BLOCKED_KEYWORDS:
+        if kw.lower() in text:
+            print(f"  Blocked by keyword '{kw}': {article['title'][:60]}...")
+            return True
+    return False
 
 
 def score_article(article):
